3cc3c48cf88b3759feb94a32f9729191fc46563d
vipcxj <vipcxj@126.com>
2024-03-27 20:10:37 +0800

    wip

--- cpp/client/src/public/cfgo/gst/error.hpp
+++ cpp/client/src/public/cfgo/gst/error.hpp
@@ -2,12 +2,15 @@
 #define _CFGO_GST_ERROR_HPP_
 
 #include "cfgo/gst/error.h"
+#include <exception>
+#include <string>
 
 namespace cfgo
 {
     namespace gst
     {
-        
+        GError * create_gerror_timeout(const std::string & message, bool trace);
+        GError * create_gerror_from_except(const std::exception_ptr & except);
     } // namespace gst
     
 } // namespace cfgo
