6ea0b8cb1822b12f06da8251648b6c50088bb52a
vipcxj <vipcxj@126.com>
2024-06-05 20:13:48 +0800

    (wip) add go client

--- client/client.go
+++ client/client.go
@@ -10,16 +10,21 @@ import (
 	"github.com/vipcxj/conference.go/model"
 )
 
+type ParticipantCb = func(participant *model.Participant)
+
 type Client interface {
+	OnParticipantJoin(cb ParticipantCb)
+	OnParticipantLeave(cb ParticipantCb)
 	OnCustomMsg(evt string, cb CustomMsgCb)
 	SendCustomMsg(timeout time.Duration, ack bool, evt string, content string, to string)
 }
 
 type WebsocketClient struct {
-	signal          Signal
-	participant_mux sync.Mutex
-	participants    map[string]model.Participant
-	participant_cb  func(participants map[string]model.Participant)
+	signal               Signal
+	participant_mux      sync.Mutex
+	participants         map[string]model.Participant
+	participant_join_cb  ParticipantCb
+	participant_leave_cb ParticipantCb
 }
 
 func NewWebsocketClient(ctx context.Context, conf *WebSocketSignalConfigure, engine *nbhttp.Engine) Client {
@@ -35,15 +40,29 @@ func NewWebsocketClient(ctx context.Context, conf *WebSocketSignalConfigure, eng
 		}
 		client.participant_mux.Lock()
 		defer client.participant_mux.Unlock()
-		client.participants[msg.UserId] = model.Participant{
+		_, exist := client.participants[msg.UserId]
+		participant := model.Participant{
 			Id:   msg.UserId,
 			Name: msg.UserName,
 		}
-		if client.participant_cb != nil {
-			client.participant_cb(client.participants)
+		client.participants[msg.UserId] = participant
+		if !exist && client.participant_join_cb != nil {
+			client.participant_join_cb(&participant)
 		}
 	})
 	return client
+}
+
+func (c *WebsocketClient) OnParticipantJoin(cb ParticipantCb) {
+	c.participant_mux.Lock()
+	defer c.participant_mux.Unlock()
+	c.participant_join_cb = cb
+}
+
+func (c *WebsocketClient) OnParticipantLeave(cb ParticipantCb) {
+	c.participant_mux.Lock()
+	defer c.participant_mux.Unlock()
+	c.participant_leave_cb = cb
 }
 
 func (c *WebsocketClient) OnCustomMsg(evt string, cb CustomMsgCb) {
